91e1c1c105886a6127222d8d60a10091e5a5e289
mickie2k <63552271+mickie2k@users.noreply.github.com>
2024-09-15 17:49:33 +0700

    Add components

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -20,10 +20,10 @@
 				<a href="/">Home</a>
 			</li>
 			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/about">About</a>
+				<a href="/aboutExample">About</a>
 			</li>
 			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
-				<a href="/sverdle">Sverdle</a>
+				<a href="/sverdleExample">Sverdle</a>
 			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
