4c707883a07e0b8228cae368778d3b635e626134
Garaiiiii <toriyakikuributa@gmail.com>
2024-10-06 12:29:38 +0700

    try to merge with main

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
 	import welcome from '$lib/images/svelte-welcome.webp';
 	import welcome_fallback from '$lib/images/svelte-welcome.png';
 	import Banner from './Banner.svelte';
+	import InputRange from './inputRange.svelte';
 </script>
 
 <svelte:head>
@@ -12,7 +13,17 @@
 
 <section>
 	<Banner/>
-	<h1 class="text-3xl font-bold underline text-red-950">Hello world!</h1>
+	<div class="flex flex-row">
+		<div class=" border-2 rounded-md">
+			<h1>fliter</h1>
+			<hr width="100%" size="2" />
+			<InputRange/>
+		</div>
+		<div>
+			<h1 class="text-3xl font-bold text-red-950">Results</h1>
+		</div>
+	</div>
+	<!-- <h1 class="text-3xl font-bold underline text-red-950">Hello world!</h1> -->
 
 	
 </section>
